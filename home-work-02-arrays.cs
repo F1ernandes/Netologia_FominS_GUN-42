7822f3775d9ce48385d891bacd07a699fa7be901
Sergey Fomin <mail@fominss.ru>
2026-02-25 13:09:49 +0300

    New logic to up homework to GIT

---
+++ home-work-02-arrays.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetologyGIT
+{
+    internal class home_work_02_arrays
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+}
