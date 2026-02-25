92659be2e20d4984ee05da8696ec3affc58c9a0f
Sergey Fomin <mail@fominss.ru>
2026-02-25 15:31:19 +0300

    Fibonachy numbers is done

--- Netology02HomeWorkArrays/Program.cs
+++ Netology02HomeWorkArrays/Program.cs
@@ -5,15 +5,31 @@ using System.Linq.Expressions;
 
 namespace Lessons
 {
-    /*Changes For Test Commit*/
-
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi Everybody");
+
+            /*Fibonachi Numbers*/
+            int[] fibonachiNumbers = new int[15];
+
+            for (int i = 0; i < fibonachiNumbers.Length; i++)
+            {
+                switch (i)
+                {
+                    case 0: case 1: fibonachiNumbers[i] = i; break;
+                    default: fibonachiNumbers[i] = fibonachiNumbers[i - 1] + fibonachiNumbers[i - 2]; break;
+                }
+            }
+            int indexKey = 0;
+            foreach (int fibonachiNumber in fibonachiNumbers)
+            {
+                indexKey++; //Numeracija po chelovecheski 
+                Console.WriteLine(indexKey + " => " + fibonachiNumber);
+            }
+
+
+
         }
-
-
     }
 }
