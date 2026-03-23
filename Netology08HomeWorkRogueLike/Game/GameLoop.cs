b284a028f26e092c155d4e1229730e04a308297a
Sergey Fomin <mail@fominss.ru>
2026-03-23 17:48:32 +0300

    OOP

---
+++ Netology08HomeWorkRogueLike/Game/GameLoop.cs
@@ -0,0 +1,125 @@
+﻿using GamePrototype.Combat;
+using GamePrototype.Dungeon;
+using GamePrototype.Units;
+using GamePrototype.Utils;
+
+namespace GamePrototype.Game
+{
+    public sealed class GameLoop
+    {
+        private Unit _player;
+        private DungeonRoom _dungeon;
+        private readonly CombatManager _combatManager = new CombatManager();
+        
+        public void StartGame() 
+        {
+            Initialize();
+            Console.WriteLine("Entering the dungeon");
+            StartGameLoop();
+        }
+
+        #region Game Loop
+
+        private void Initialize()
+        {
+            Console.WriteLine("Welcome, player!");
+            _dungeon = DungeonBuilder.BuildDungeon();
+            Console.WriteLine("Enter your name");
+            _player = UnitFactoryDemo.CreatePlayer(Console.ReadLine());
+            Console.WriteLine($"Hello {_player.Name}");
+        }
+
+        private void StartGameLoop()
+        {
+            var currentRoom = _dungeon;
+            
+            while (currentRoom.IsFinal == false) 
+            {
+                StartRoomEncounter(currentRoom, out var success);
+                if (!success) 
+                {
+                    Console.WriteLine("Game over!");
+                    return;
+                }
+                DisplayRouteOptions(currentRoom);
+                while (true) 
+                {
+                    string commandOfUser = Console.ReadLine();
+                    
+                    if (Enum.TryParse<Direction>(commandOfUser, out var direction))
+                    {
+                        currentRoom = currentRoom.Rooms[direction];
+                        break;
+                    }
+                    else
+                    {
+                        switch (commandOfUser)
+                        {
+                            case "show": case "Show":
+                                _player.ShowPlayerInventare();
+                                break;
+                            case "grindstone":
+                                if(!_player.HasHeItem("Stone")){ Console.WriteLine("You haven't grindstone!"); break;  }
+                                _player.UseItem("Stone");
+                                break;
+                            default:
+                                Console.WriteLine("Wrong command!");
+                                break;
+                        }
+                        
+                    }
+                }
+            }
+            Console.WriteLine($"Congratulations, {_player.Name}");
+            Console.WriteLine("Result: ");
+            Console.WriteLine(_player.ToString());
+        }
+
+        private void StartRoomEncounter(DungeonRoom currentRoom, out bool success)
+        {
+            success = true;
+            
+            if (currentRoom.Loot != null) 
+            {
+                Console.WriteLine("Я нашёл в комнате " + currentRoom.Loot);
+                _player.AddItemToInventory(currentRoom.Loot);
+            }
+            if (currentRoom.Enemy != null) 
+            {
+                if (_combatManager.StartCombat(_player, currentRoom.Enemy) == _player)
+                {
+                    _player.HandleCombatComplete();
+                    LootEnemy(currentRoom.Enemy);
+                }
+                else 
+                {
+                    success = false;
+                }
+            }
+
+            void LootEnemy(Unit enemy)
+            {
+                _player.AddItemsFromUnitToInventory(enemy);
+            }
+        }
+
+        private void DisplayRouteOptions(DungeonRoom currentRoom)
+        {
+            Console.Write("\n");
+            Console.WriteLine("Where to go?");
+            foreach (var room in currentRoom.Rooms)
+            {
+                Console.Write($"{room.Key} - {(int) room.Key}\t");
+            }
+            Console.Write("\n");
+            Console.WriteLine("Type \"show\" - To show your equipment");
+
+            Console.Write("\n");
+            Console.WriteLine("Type \"grindstone\" - To use grindstone");
+
+        }
+
+
+        #endregion
+    }
+}
---
+++ Netology09DelegatesAndEvents/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using System.Linq.Expressions;
+using Netology09DelegatesAndEvents;
+
+namespace Netology09DelegatesAndEvents
+{
+    public delegate string ToStringConvert(Test param);
+    public class Test
+    {
+        public Test(string someString) => SomeString = someString;
+        private string SomeString { get; }
+        public string Method(Test param) => param.ToString();
+        public override string ToString()
+        {
+            return $"This is the Test class {SomeString}";
+        }
+    }
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            var test = new Test("Netology");
+            var del = new ToStringConvert(test.Method);
+            Console.WriteLine(del(test));
+        }
+    }
+}
